89aef55e41f1e37a29b4a89e72b4291093c21f5a
Ekaterina Pecherskaya <Pechorskaya.K8@gmail.com>
2023-12-13 14:55:36 +0200

    Initial Commit

---
+++ TrafficLight_SwiftUI/TrafficLight_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_SwiftUIApp.swift
+//  TrafficLight_SwiftUI
+//
+//  Created by Chorrs on 13.12.23.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
